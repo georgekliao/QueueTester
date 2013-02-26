5c46f2ac0b6136d866dba8ec89eea265abeefbc2
George Liao <gliao@sailthru.com>
2013-02-25 21:42:06 -0500

    working kafka queue

--- src/main/java/com/sailthru/queuetester/App.java
+++ src/main/java/com/sailthru/queuetester/App.java
@@ -1,6 +1,7 @@
 package com.sailthru.queuetester;
 
 import com.sailthru.queuetester.queue.IQueue;
+import com.sailthru.queuetester.queue.kafka.KafkaQueue;
 import com.sailthru.queuetester.queue.rabbitmq.RabbitQueue;
 import com.sailthru.queuetester.queue.redis.RedisQueue;
 
@@ -8,7 +9,8 @@ public class App {
 
     public static void main(String[] args) throws Exception {
 //        RedisQueue redisQueue = new RedisQueue("test_queue");
-        IQueue queue = new RabbitQueue("test_queue");
+//        IQueue queue = new RabbitQueue("test_queue");
+        IQueue queue = new KafkaQueue("test_queue");
 
         for (int i = 0; i < 100; i++) {
             System.out.println("Publishing " + i);

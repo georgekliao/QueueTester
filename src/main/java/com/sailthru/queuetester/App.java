9d869cebb0fb6e94db11c754cf788008cb151f74
Max Englander <menglander@sailthru.com>
2013-05-28 15:45:37 -0400

    parameterize host (-h)

--- src/main/java/com/sailthru/queuetester/App.java
+++ src/main/java/com/sailthru/queuetester/App.java
@@ -6,6 +6,7 @@ import com.sailthru.queuetester.test.AbstractTest;
 import com.sailthru.queuetester.test.TestFactory;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.apache.commons.cli.BasicParser;
@@ -17,6 +18,7 @@ public class App {
     public static void main(String[] args) throws Exception {        
         Options options = new Options(); 
         options.addOption("c", true, "concurrency [default 4]");
+        options.addOption("h", true, "host [default localhost]");
         options.addOption("n", true, "number of messages [default 1000]");        
         options.addOption("q", true, "queue type [kafka, redis, amq, rmq]");
         options.addOption("s", true, "message size [default 1024]");
@@ -26,26 +28,32 @@ public class App {
         CommandLine commandLine = commandLineParser.parse(options, args);
         
         int concurrency = Integer.parseInt(commandLine.getOptionValue("c", "4"));
+        final String host = commandLine.getOptionValue("h", "localhost");
         final String queueType = commandLine.getOptionValue("q", "amq");
         final int messageSize = Integer.parseInt(commandLine.getOptionValue("s", "1024"));
         final int numMessages = Integer.parseInt(commandLine.getOptionValue("n", "1000"));        
         final String testType = commandLine.getOptionValue("t", "pub");
         
-        ExecutorService executor = Executors.newFixedThreadPool(4 <= concurrency ? 4 : concurrency);
-
+        int availableProcessors = Runtime.getRuntime().availableProcessors();
+        ExecutorService executor = Executors.newFixedThreadPool(availableProcessors <= concurrency ? availableProcessors : concurrency);        
+        
         for (int i = 0; i < concurrency; i++) {
             executor.submit(new Runnable() {
                 public void run() {
                     try {
-                        IQueue queue = QueueFactory.create(queueType, "test_queue");
+                        IQueue queue = QueueFactory.create(queueType, host, "test_queue");
                         AbstractTest test = TestFactory.create(testType, numMessages, messageSize);
                         test.setQueue(queue);                    
-                        test.execute();
+                        test.execute();                        
                     } catch (Exception ex) {
-                        Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);                        
-                    }
+                        Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
+                    }                    
                 }
             });
         }
+        
+        // See http://stackoverflow.com/questions/1250643/how-to-wait-for-all-threads-to-finish-using-executorservice
+        executor.shutdown();
+        executor.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);        
     }
 }
